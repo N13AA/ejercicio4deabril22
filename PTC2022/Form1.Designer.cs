9b416b23a54710e6ea20710315ab00e93648ed1f
N13AA <nestorjosuearana68@gmail.com>
2022-05-04 14:55:23 -0500

    rr

--- PTC2022/Form1.Designer.cs
+++ PTC2022/Form1.Designer.cs
@@ -28,32 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button2
+            // label1
             // 
-            this.button2.Location = new System.Drawing.Point(306, 148);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione el menu a utilizar.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
