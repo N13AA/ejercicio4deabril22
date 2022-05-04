c9d8e1593ee9ce00d7b59b24d6507c602640c3a1
N13AA <nestorjosuearana68@gmail.com>
2022-05-04 15:05:39 -0500

    prom

--- PTC2022/Form1.cs
+++ PTC2022/Form1.cs
@@ -16,5 +16,12 @@ namespace PTC2022
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 promedios = new Form2();
+            promedios.Show();
+        }
     }
 }
